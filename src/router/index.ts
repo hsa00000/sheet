b6270a3b8268c4674555b15cb9bd6860514975bf
hsa00000 <hsa00000@gmail.com>
2025-04-10 16:51:22 +0800

    switch to using static imports for views and change history mode to createWebHashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+// 靜態 import 所有的 view
+import EditView from '../views/EditView.vue'
+import PrintView from '../views/PrintView.vue'
+import GuideView from '../views/GuideView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/EditView.vue'),
+      component: EditView,
     },
     {
       path: '/edit',
       name: 'edit',
-      component: () => import('../views/EditView.vue'),
+      component: EditView,
     },
     {
       path: '/print',
       name: 'print',
-      component: () => import('../views/PrintView.vue'),
+      component: PrintView,
     },
     {
       path: '/guide',
       name: 'guide',
-      component: () => import('../views/GuideView.vue'),
+      component: GuideView,
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      component: AboutView,
     },
   ],
 })
