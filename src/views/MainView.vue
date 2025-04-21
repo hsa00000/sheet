fa8e2ad76537dc16dcac104afd860477e9620f01
hsa00000 <hsa00000@gmail.com>
2025-04-21 09:19:15 +0800

    feat: wrap toolbar in a v-col for improved layout in MainView

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,39 +1,41 @@
 <template>
   <v-container class="d-flex justify-center align-center">
     <v-row>
-      <v-toolbar class="no-print px-2 w-100">
-        <v-slide-group show-arrows class="flex-grow-1">
-          <v-slide-group-item>
-            <v-btn to="/edit" class="ma-1">編輯模式</v-btn>
-          </v-slide-group-item>
-          <v-slide-group-item>
-            <v-btn to="/print" class="ma-1">列印模式</v-btn>
-          </v-slide-group-item>
-          <v-slide-group-item>
-            <v-btn to="/guide" class="ma-1">使用教學</v-btn>
-          </v-slide-group-item>
-          <v-slide-group-item>
-            <v-btn to="/about" class="ma-1">關於本站</v-btn>
-          </v-slide-group-item>
-          <!-- 更多按鈕 -->
-        </v-slide-group>
+      <v-col>
+        <v-toolbar class="no-print px-2 w-100">
+          <v-slide-group show-arrows class="flex-grow-1">
+            <v-slide-group-item>
+              <v-btn to="/edit" class="ma-1">編輯模式</v-btn>
+            </v-slide-group-item>
+            <v-slide-group-item>
+              <v-btn to="/print" class="ma-1">列印模式</v-btn>
+            </v-slide-group-item>
+            <v-slide-group-item>
+              <v-btn to="/guide" class="ma-1">使用教學</v-btn>
+            </v-slide-group-item>
+            <v-slide-group-item>
+              <v-btn to="/about" class="ma-1">關於本站</v-btn>
+            </v-slide-group-item>
+            <!-- 更多按鈕 -->
+          </v-slide-group>
 
-        <v-btn
-          variant="tonal"
-          rounded="xl"
-          prepend-icon="mdi-content-copy"
-          class="ms-2"
-          @click="logShareUrl"
-        >
-          分享
-        </v-btn>
-      </v-toolbar>
+          <v-btn
+            variant="tonal"
+            rounded="xl"
+            prepend-icon="mdi-content-copy"
+            class="ms-2"
+            @click="logShareUrl"
+          >
+            分享
+          </v-btn>
+        </v-toolbar>
 
-      <router-view v-slot="{ Component }">
-        <KeepAlive include="EditView,PrintView">
-          <component :is="Component" />
-        </KeepAlive>
-      </router-view>
+        <router-view v-slot="{ Component }">
+          <KeepAlive include="EditView,PrintView">
+            <component :is="Component" />
+          </KeepAlive>
+        </router-view>
+      </v-col>
     </v-row>
   </v-container>
 </template>
