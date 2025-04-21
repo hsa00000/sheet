b0399eccbad248696a092de3badbcd9212c6135c
hsa00000 <hsa00000@gmail.com>
2025-04-21 10:23:27 +0800

    fix: correct label from '簽到' to '簽名' in BASE_HEADERS

--- src/const/const.ts
+++ src/const/const.ts
@@ -6,7 +6,7 @@ export const BASE_HEADERS: Header[] = [
   { title: '學號', value: 'id', width: '100px', align: 'center' },
   { title: '單位', value: 'department', width: '100px', align: 'center' },
   { title: '姓名', value: 'name', width: '100px', align: 'center' },
-  { title: '簽到', value: 'sign', width: '100px', align: 'center' },
+  { title: '簽名', value: 'sign', width: '100px', align: 'center' },
   { title: '用餐', value: 'food', width: '100px', align: 'center' },
 ]
 
